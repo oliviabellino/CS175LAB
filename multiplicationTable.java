9282d3a42119f1e19ae77b5d03b4443598719459
Olivia Bellino <71726052+oliviabellino@users.noreply.github.com>
2021-04-11 20:13:56 -0400

    Add files via upload

---
+++ multiplicationTable.java
@@ -0,0 +1,20 @@
+
+
+public class multiplicationTable {
+
+	public static void main(String[] args) {
+
+	       for(int a=1;a<=10;a++)
+	       {
+	    	   for(int x=1; x<=10; x++)
+	    	   {
+	    		   System.out.printf("%5d",a*x);
+	    	   }
+	    	   System.out.println();
+	       }
+		}
+
+
+
+	}
+
